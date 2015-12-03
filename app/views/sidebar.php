f20929e38d5ceea7c046bf52bf33aa2b8043474f
iceclaser <mikeweer@hotmail.com>
2015-12-03 11:09:16 +0100

    twitter

--- app/views/sidebar.php
+++ app/views/sidebar.php
@@ -8,5 +8,6 @@
     <div id="bigDynamic">
         <p></p>
     </div>
-    <img src="img/sidebar_twitter.png" alt=""/>
+    <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/hashtag/tourdefrance" data-widget-id="672338195359625216">Tweets over #tourdefrance</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
