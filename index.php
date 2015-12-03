4a1d0f7b88332c94a3ee2297b87f4ee0ca54b50c
goartur <kolo1234>
2015-12-03 20:49:14 +0200

    modified:   .idea/workspace.xml 	modified:   index.php

--- index.php
+++ index.php
@@ -5,14 +5,3 @@ require 'app/views/sidebar.php';
 require 'app/views/content.php';
 switch_include::getInstance()->getPage(Input::get('page'));
 require 'app/views/footer.php';
-
-$filename = 'songs.txt';
-$contents = file($filename);
-$array = [];
-foreach($contents as $line) {
-    $array[] = explode('-',$line,4);
-}
-
-foreach($array as $value) {
-    connection::getInstance()->query("INSERT INTO songs(`hitNr`,`name`,`artist`,`day`) VALUES ('$value[0]','$value[1]','$value[2]','$value[3]')");
-}
