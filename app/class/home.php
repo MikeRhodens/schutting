58f34f27de505ae28b6e93145f70c75f7657ed79
iceclaser <mikeweer@hotmail.com>
2015-12-07 17:45:13 +0100

    pagination sorted

--- app/class/home.php
+++ app/class/home.php
@@ -17,7 +17,7 @@ class home
 
     public function getSongs($date_id)
     {
-        $query = 'SELECT * FROM songs WHERE day = ' . $date_id.'';
+        $query = 'SELECT * FROM songs WHERE day = ' . $date_id.' order by id desc';
         $data = $this->connection->query($query);
         return $data;
     }
